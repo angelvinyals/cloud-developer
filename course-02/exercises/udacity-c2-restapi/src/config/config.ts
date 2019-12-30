d0e1ca42f4d2a6e928a6834bb856b80db9e34876
admin <admin@admin.ad>
2019-12-30 10:58:15 +0100

    update config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramtest1dev",
+    "password": "udagramtest1dev",
+    "database": "udagramtest1dev",
+    "host": "udagramtest1dev.cqa6hhhwnkcf.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-ruttner-dev"
   },
